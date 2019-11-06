c5c646e1054294768a6e0db2043a7029bda8f14b
ronan <g00342393@gmit.ie>
2019-11-06 13:42:15 +0000

    Flask server now returns mongo data

--- weather-api/weather-server/weather-server-flask/swagger_server/controllers/users_controller.py
+++ weather-api/weather-server/weather-server-flask/swagger_server/controllers/users_controller.py
@@ -4,6 +4,9 @@ import six
 from ..models.weather_history import WeatherHistory  # noqa: E501
 from ..models.weather_history import DataPoint
 from .. import util
+import pymongo
+import gridfs
+from flask import abort
 
 
 def get_weather_history(area, dataset):  # noqa: E501
@@ -18,5 +21,18 @@ def get_weather_history(area, dataset):  # noqa: E501
 
     :rtype: WeatherHistory
     """
-    test = WeatherHistory("Test desc", 1, [DataPoint("15-aug-2012 17:00", 17, 6)])
-    return test
+    try:
+        # test = WeatherHistory("Test desc", 1, [DataPoint("15-aug-2012 17:00", 17, 6)])
+
+        # user Ronan is read-only for security
+        mongo = pymongo.MongoClient("mongodb://Ronan:4thyearproject2019@ds243518.mlab.com:43518/weather")
+        db = mongo["weather"]
+        coll = db.get_collection(area)
+        doc = list(coll.find({}))[dataset]
+        del doc["_id"]
+        # this line seems redundant, you can just return the doc dict instead
+        response = WeatherHistory(**doc)
+
+        return response
+    except:
+        abort(404)
