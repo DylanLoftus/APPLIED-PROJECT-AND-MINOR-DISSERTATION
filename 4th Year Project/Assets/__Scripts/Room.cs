62b5713e1569de46353ffef7c284d131e190d5d2
Ronan Hanley <g00342393@gmit.ie>
2019-11-11 11:43:02 +0000

    Floor colour on a blue/red gradient based on temperature

--- 4th Year Project/Assets/__Scripts/Room.cs
+++ 4th Year Project/Assets/__Scripts/Room.cs
@@ -25,19 +25,18 @@ public class Room : MonoBehaviour
     {
         roomTemperature = gameManager.outsideTemp + 8;
 
-        if(gameManager.outsideTemp > 15.0)
-        {
-            // Change colour of walls to blue
-            var roomRenderer = room.GetComponentInChildren<Renderer>();
-            roomRenderer.material.SetColor("_Color", Color.blue);
+        // change floor color of floor to gradient between blue/red based on temperature
+        float floorTemp = 12;
+        float ceilTemp = 16;
+        float tempScale = ceilTemp - floorTemp;
 
-        }
-        else
-        {
-            // Change colour of walls to red
-            var roomRenderer = room.GetComponentInChildren<Renderer>();
-            roomRenderer.material.SetColor("_Color", Color.red);
-        }
+        float tempGradient = Mathf.Clamp((gameManager.outsideTemp - floorTemp) / tempScale, 0, 1);
+        float red = tempGradient;
+        float blue = (1 - tempGradient);
+
+
+        var roomRenderer = room.GetComponentInChildren<Renderer>();
+        roomRenderer.material.SetColor("_Color", new Color(red, 0, blue));
     }
 
 
