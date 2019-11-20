f889c5bad9c9dac21156c333d00efc385b520701
ronan <g00342393@gmit.ie>
2019-11-20 14:47:29 +0000

    Restructured flask server to work with uWSGI

---
+++ weather-api/weather-server/weather-server-flask/swagger_server/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import connexion
+
+import encoder
+
+app = connexion.App(__name__, specification_dir='./swagger/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('swagger.yaml', arguments={'title': 'Weather API'}, pythonic_params=True)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
