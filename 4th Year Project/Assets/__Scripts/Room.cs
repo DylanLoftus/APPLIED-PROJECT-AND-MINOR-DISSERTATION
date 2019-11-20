af2510e050265f30a7a386f3b4fe83648ebe8395
Ronan-H <g00342393@gmit.ie>
2019-11-20 19:20:59 +0000

    Windows speed up heat loss/gain from outside

--- 4th Year Project/Assets/__Scripts/Room.cs
+++ 4th Year Project/Assets/__Scripts/Room.cs
@@ -28,21 +28,23 @@ public class Room : MonoBehaviour
     void Start()
     {
         gameManager = GameObject.FindObjectOfType<GameManager>();
-        roomObject = GameObject.FindObjectOfType<Room>();
-        window = GameObject.FindObjectOfType<Window>();
+        roomObject = gameObject.GetComponent<Room>();
+        window = gameObject.GetComponentInChildren<Window>();
 
         adjRooms = new Room[] {roomNorth, roomSouth, roomEast, roomWest};
+
+        roomTemperature = 8;
     }
 
     // Update is called once per frame
     void Update()
     {
         // change floor color of floor to gradient between blue/red based on temperature
-        float floorTemp = 12;
-        float ceilTemp = 16;
+        float floorTemp = 8;
+        float ceilTemp = 18;
         float tempScale = ceilTemp - floorTemp;
 
-        float tempGradient = Mathf.Clamp((gameManager.outsideTemp - floorTemp) / tempScale, 0, 1);
+        float tempGradient = Mathf.Clamp((roomTemperature - floorTemp) / tempScale, 0, 1);
         float red = tempGradient;
         float blue = (1 - tempGradient);
         
@@ -59,7 +61,7 @@ public class Room : MonoBehaviour
         float numWallsExposed = adjRooms.Count(r => r == null);
 
         // compute heat rate to the outside (10x faster if window is open)
-        float heatLossRate = (numWallsExposed * (window.isOpen ? 10 : 2)) / 20f;
+        float heatLossRate = (numWallsExposed * (window.isOpen ? 10 : 2)) / 80f;
         //Debug.Log("Heat loss rate: " + heatLossRate);
 
         // change room temperature towards temperature outside
