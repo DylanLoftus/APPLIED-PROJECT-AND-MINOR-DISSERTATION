359d64ff465a74f621e279e5d4c1950c6c5a436d
Ronan Hanley <g00342393@gmit.ie>
2019-11-25 21:42:52 +0000

    Temperature exchange between rooms (#5)
    * Temperature exchange between rooms (untested)
    
    * Temp exchange between rooms now working

---
+++ 4th Year Project/Assets/__Scripts/AdjRooms.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+public class AdjRooms
+{
+    [SerializeField]
+    private Room roomNorth;
+    [SerializeField]
+    private Room roomSouth;
+    [SerializeField]
+    private Room roomEast;
+    [SerializeField]
+    private Room roomWest;
+
+    public Room[] GetAdjList()
+    {
+        return new Room[] { roomNorth, roomSouth, roomEast, roomWest };
+    }
+
+    public Room[] GetConnectedRooms()
+    {
+        return GetAdjList().Where(r => r != null).ToArray();
+    }
+
+    public int NumConnectedRooms()
+    {
+        return GetAdjList().Count(r => r != null);
+    }
+
+    public int NumSidesNotConnected()
+    {
+        return GetAdjList().Count(r => r == null);
+    }
+}
