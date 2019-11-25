1068953323027a05937e58eef92158a2fe84177a
DylanLoftus <dylanloftus@hotmail.com>
2019-11-25 14:40:48 +0000

    added cardboard vr and android packages, got temp text display as well

---
+++ 4th Year Project/Assets/__Scripts/TempDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TempDisplay : MonoBehaviour
+{
+    Room room;
+    TextMeshPro currentRoomTemp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        room = GetComponentInParent<Room>();
+        currentRoomTemp = GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rounded = (float)Math.Round(room.roomTemperature * 100f) / 100f;
+        currentRoomTemp.text = rounded.ToString();
+    }
+}
