fa6bd0cf61c954987f4cfd30a611f1de9edefb22
Ronan-H <g00342393@gmit.ie>
2019-12-05 16:32:16 +0000

    Added radiators (no effect on temps yet)

---
+++ 4th Year Project/Assets/__Scripts/Radiator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radiator : MonoBehaviour
+{
+    public bool isOn;
+
+    GameManager gameManager;
+    Room room;
+
+    void Start()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        room = GameManager.FindObjectOfType<Room>();
+        isOn = false;
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            if (collision.tag == "Player")
+            {
+                isOn = !isOn;
+
+                Debug.Log("Radiator on: " + isOn);
+            }
+        }
+    }
+}
