914128c833e4c80fe637272dc3fa2295964d42a1
Ronan-H <g00342393@gmit.ie>
2019-12-05 18:38:07 +0000

    Added basic movement for mobile VR

---
+++ 4th Year Project/Assets/__Scripts/WalkWhereLooking.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkWhereLooking : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    [SerializeField]
+    private Transform cameraTransform;
+    [SerializeField]
+    private float moveSpeed = 2.5f;
+    
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // move in the direction that the camera's facing
+            rigidbody.velocity = cameraTransform.forward * moveSpeed;
+        }
+    }
+}
