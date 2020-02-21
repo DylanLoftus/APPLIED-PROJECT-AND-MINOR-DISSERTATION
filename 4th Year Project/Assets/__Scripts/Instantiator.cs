b9eafdad6cad3365b515cda143f8bdb2b2b98b14
DylanLoftus <dylanloftus@hotmail.com>
2020-02-21 18:11:56 +0000

    implemented room spawning for both hallway types

--- 4th Year Project/Assets/__Scripts/Instantiator.cs
+++ 4th Year Project/Assets/__Scripts/Instantiator.cs
@@ -13,7 +13,11 @@ public class Instantiator : MonoBehaviour
     [SerializeField]
     private Transform hallwaySpawn;
 
+    private Transform roomSpawn;
+
     private int turnCount = 0;
+    private int roomCount;
+    private int maxRoom;
 
 
     [SerializeField]
@@ -24,8 +28,7 @@ public class Instantiator : MonoBehaviour
     private GameObject sideWallS;
     private GameObject doorCover;
 
-    public bool rightRoomCreate = true;
-    public bool leftRoomCreate = true;
+    public bool hallwayFull = true;
 
     private GameObject newRoom;
     private GameObject newHallway;
@@ -40,32 +43,35 @@ public class Instantiator : MonoBehaviour
 
     private void CreateHallway(GameObject hallwayChoice)
     {
-        leftRoomCreate = false;
-        rightRoomCreate = false;
+        // For creating rooms and checking if hallway is full.
+        roomCount = 1;
+        hallwayFull = false;
 
         wallDestroy = hallway.transform.Find("WallDestroy").gameObject;
         wallDestroy.SetActive(false);
         
+        // Set the hallway spawn locations and their respective room allocation.
         if(hallwayChoice == hallwayL)
         {
             hallwaySpawn = hallway.transform.Find("SpawnpointHallwayL").transform;
+            maxRoom = 4;
         }
         else
         {
             hallwaySpawn = hallway.transform.Find("SpawnpointHallway").transform;
+            maxRoom = 2;
         }
         
         newHallway = Instantiate(hallwayChoice, new Vector3(hallwaySpawn.transform.position.x, hallwaySpawn.transform.position.y, hallwaySpawn.transform.position.z), Quaternion.identity);
 
+        // If the hallway is an L hallway we'll need to rotate the second one and ever subsequent one after that 90 degrees.
         if(hallwayChoice == hallwayL)
         {
             if(turnCount > 0)
             {
                 newHallway.transform.Rotate(0, turnCount * 90, 0);
             }
-           
             turnCount++;
-            
         }
 
         wallInsDestroy = newHallway.transform.Find("WallInsDestroy").gameObject;
@@ -98,10 +104,22 @@ public class Instantiator : MonoBehaviour
         CheckRooms(0);
     }
 
+    // Creates a room Game Object
     private GameObject CreateRoom(GameObject room, int rotation)
     {
-        newRoom = Instantiate(roomR, new Vector3(room.transform.position.x, room.transform.position.y, room.transform.position.z + 10), Quaternion.identity);
+        // Set the room's spawn location.
+        roomSpawn = hallway.transform.Find("SpawnpointRoom" + roomCount.ToString()).transform;
+        newRoom = Instantiate(roomR, new Vector3(roomSpawn.transform.position.x, roomSpawn.transform.position.y, roomSpawn.transform.position.z), Quaternion.identity);
         newRoom.transform.rotation = Quaternion.Euler(new Vector3(0, rotation, 0));
+
+        // Increment room count.
+        roomCount++;
+
+        // If the room count exceeds the maximum number of rooms for a hallway the hallway is full.
+        if (roomCount > maxRoom)
+        {
+            hallwayFull = true;
+        }
 
         Room newRoomScript = newRoom.GetComponent<Room>();
         newRoomScript.adjRooms.reset();
@@ -112,7 +130,8 @@ public class Instantiator : MonoBehaviour
 
     public void CheckRooms(int choice)
     {
-        if (leftRoomCreate == true && rightRoomCreate == true)
+        // If all rooms have been filled create a new hallway.
+        if (hallwayFull == true)
         {
             if (choice == 1)
             {
@@ -125,11 +144,30 @@ public class Instantiator : MonoBehaviour
         }
         else if (choice == 0)
         {
-            if (leftRoomCreate == false && rightRoomCreate == false)
+            // If the hallway is not full we have rooms to put in. Depending on how many rooms have already been created the rotations need to be set.
+            // TODO: make rotation relative to hallway
+            if (hallwayFull == false)
             {
+                switch (roomCount)
+                {
+                    case 1:
+                        roomL = CreateRoom(roomL, 90);
+                        break;
+                    case 2:
+                        roomL = CreateRoom(roomL, -90);
+                        break;
+                    case 3:
+                        roomL = CreateRoom(roomL, 180);
+                        break;
+                    case 4:
+                        roomL = CreateRoom(roomL, 0);
+                        break;
+
+                }
+            }
+                /*
                 Room oldRoom = roomL.GetComponent<Room>();
                 roomL = CreateRoom(roomL, 90);
-                leftRoomCreate = true;
 
                 // update room adjacencies
                 Room newRoom = roomL.GetComponent<Room>();
@@ -158,7 +196,6 @@ public class Instantiator : MonoBehaviour
                 Room newRoom = roomR.GetComponent<Room>();
                 doorCover.SetActive(false);
                 door.SetActive(true);
-                rightRoomCreate = true;
 
                 // update room adjacencies
                 Room hallwayRoom = hallway.GetComponent<Room>();
@@ -177,6 +214,11 @@ public class Instantiator : MonoBehaviour
                 // register new room with GameManager
                 gameManager.addRoom(newRoom);
             }
+            */
+        }
+        else
+        {
+            Debug.Log("Invalid choice");
         }
     }
 
--- 4th Year Project/Assets/__Scripts/OculusControllers.cs
+++ 4th Year Project/Assets/__Scripts/OculusControllers.cs
@@ -20,11 +20,13 @@ public class OculusControllers : MonoBehaviour
 
         if ((OVRInput.GetDown(OVRInput.RawButton.A) == true) || (Input.GetKeyUp("space") == true))
         {
-            if(instantiator.rightRoomCreate == false || instantiator.leftRoomCreate == false)
+            // If the hallway isn't full spawn in a room.
+            if(instantiator.hallwayFull == false)
             {
                 instantiator.CheckRooms(0);
             }
-            else if(instantiator.rightRoomCreate == true && instantiator.leftRoomCreate == true)
+            // If the hallway is full ask the user if they want another one.
+            else if(instantiator.hallwayFull == true)
             {
                 hallChoice.SetActive(true);
             }
