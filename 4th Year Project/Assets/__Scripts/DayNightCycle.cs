6cd402d7b52213e343d5a2a918ce772ff6ddc5c5
DylanLoftus <dylanloftus@hotmail.com>
2020-02-25 13:25:24 +0000

    added temp dynamic skybox

--- 4th Year Project/Assets/__Scripts/DayNightCycle.cs
+++ 4th Year Project/Assets/__Scripts/DayNightCycle.cs
@@ -12,12 +12,14 @@ public class DayNightCycle : MonoBehaviour
     private Transform sunMoonStartRotation;
     private const float dayLength = 24;
     private int totalRotation = -15;
+    private MeshRenderer mr;
+    private float offsetDiv = 0.05f;
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
-
+        mr = GameObject.Find("SkyDome").GetComponent<MeshRenderer>();
         StartCoroutine(WaitTime());
         
     }
@@ -34,6 +36,7 @@ public class DayNightCycle : MonoBehaviour
     {
         sunMoonStartRotation.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         sunMoonStartRotation.transform.eulerAngles = new Vector3(360f / dayLength * timeStamp + (sunMoonRotationByHour * 2), 0, 0);
+        mr.material.mainTextureOffset = new Vector2((360f / dayLength * timeStamp + (sunMoonRotationByHour * 2)) / 1000, 0);
     }
     
     // Update is called once per frame
@@ -45,6 +48,7 @@ public class DayNightCycle : MonoBehaviour
     
     public void RotateSunAndMoon()
     {
+        mr.material.mainTextureOffset += new Vector2(.04167f, 0);
         Debug.Log(totalRotation);
         transform.RotateAround(Vector3.zero, Vector3.right, totalRotation);
         transform.LookAt(Vector3.zero);
